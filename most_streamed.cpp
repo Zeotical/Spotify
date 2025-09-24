baf7dd3fc2009802a934ebb9cef451f22573d766
Zeotical <aceyems10@gmail.com>
2025-09-24 23:03:09 +0800

    Pre-questions & answers

--- most_streamed.cpp
+++ most_streamed.cpp
@@ -2,10 +2,18 @@
 #include "most_streamed.h"
 using namespace std;
 
+void guess_pop(){
+}
 void mostStreamedSongGuess() {
-    cout << "Most Streamed Song Guess Game (Coming Soon!)\n";
+    int choice;
+    int q_no = 1;
+    cout << "\nMost Streamed Song Guess Game (Coming Soon!)\n";
     cout << "Guess The Most Streamed Song!\n";
     cout << "Select a genre:\n" ;
-    cout << "1.Pop\n2.Rock\n3.Hip-Hop\n4.R&B\n5.Mixed Genres";
+    cout << "1.Pop\n2.Rock\n3.Hip-Hop\n4.R&B\n5.Mixed Genres\n";
+    cout << "Enter your choice: ";
+    cin>> choice;
+    
+   
     
 }
